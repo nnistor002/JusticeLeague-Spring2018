b6cbd4be43db6121a30b8c30772f48d7050a5913
tman2796 <tsimmons6@ggc.edu>
2018-04-06 17:39:16 -0400

    The previous room class wouldn't print the courtyard description. Fixed it and I was over-complicating the reading it in

--- src/Room.java
+++ src/Room.java
@@ -7,30 +7,43 @@ import java.util.Scanner;
 
 public class Room {
 	static int lineNum;
-	public static void main (String [] args) throws IOException {
-//		System.out.println("Enter a Rooom number");
-//		Scanner scan = new Scanner(System.in);
-//		String userInput = scan.next();
-	
-		
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\tman2\\Desktop\\JusticeLeague-Spring2018-Section1\\src\\Room.txt"));
 
-		while (br.ready()) {
-//			String brWord = br.read()
-			String brLine = br.readLine();
-			System.out.println(brLine);
-			String line =br.readLine();
-//			System.out.println(br.readLine());
-			String[] splitLines = line.split("--");
-//			System.out.println();
-			for(String element : splitLines) {
-				System.out.println();
-				System.out.println(element);
+	public static void main(String[] args) throws IOException {
+		// System.out.println("Enter a Rooom number");
+		// Scanner scan = new Scanner(System.in);
+		// String userInput = scan.next();
+
+		String strLine = null;
+		BufferedReader br = new BufferedReader(
+				new FileReader("C:\\Users\\tman2\\Desktop\\JusticeLeague-Spring2018-Section1\\src\\Room.txt"));
+
+		try {
+			BufferedReader rd = new BufferedReader(
+					new FileReader("C:\\Users\\tman2\\Desktop\\JusticeLeague-Spring2018-Section1\\src\\Room.txt"));
+			while ((strLine = rd.readLine()) != null) {
+				// Print the content on the console
+				System.out.println(strLine);
 			}
+			rd.close();
+		} catch (Exception ex) {
+			throw new NullPointerException();
 		}
+
+		// while (br.ready()) {
+		//// String brWord = br.read()
+		// String brLine = br.readLine();
+		// System.out.println(brLine);
+		// String line =br.readLine();
+		//// System.out.println(br.readLine());
+		// String[] splitLines = line.split("--");
+		//// System.out.println();
+		// for(String element : splitLines) {
+		// System.out.println();
+		// System.out.println(element);
+		// }
+		// }
 		br.close();
 
-		
 	}
 
 }
