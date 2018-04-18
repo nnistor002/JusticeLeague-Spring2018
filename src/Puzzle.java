8833831a9dfe8954a6323f7f2c395fd9efd12b92
seanlomar225 <shutchinson1@ggc.edu>
2018-04-18 17:21:20 -0400

    added comment to puzzle class on line 122

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -120,7 +120,7 @@ public class Puzzle extends Observable {
 	}
 	
 	public String getAnswer(String a) { // String a is the answer from user 
-		if(answer.contains(a.substring(0,1).toUpperCase())) {
+		if(answer.contains(a.substring(0,1).toUpperCase())) {		////answer needs to use equal not contains
 			String r= "That's right!";
 			return r;
 		} else {
